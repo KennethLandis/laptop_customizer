90ec1d67915e73c5db95f939250ff575721967fb
Kenneth Landis <67128061+KennethLandis@users.noreply.github.com>
2021-04-23 20:53:44 -0500

    Changes based on Grader feedback

---
+++ components/Form.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Specs from './Specs';
+
+function Form(props) {
+    return (
+        <form className="main__form">
+            <h2>Customize your laptop</h2>
+            <Specs
+                features={props.features}
+                state={props.state}
+                updateFeature={props.updateFeature}
+            />
+        </form>
+        )
+};
+
+export default Form;
---
+++ components/Parts.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import slugify from 'slugify';
+
+const USCurrencyFormat = new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'USD'
+  });
+
+function Parts(props) {
+    return props.features[props.feature].map(item => {
+    const itemHash = slugify(JSON.stringify(item));
+        return (
+            <div key={itemHash} className="feature__item">
+            <input
+                type="radio"
+                id={itemHash}
+                className="feature__option"
+                name={slugify(props.feature)}
+                checked={item.name === props.state[props.feature].name}
+                onChange={e => props.updateFeature(props.feature, item)}
+            />
+            <label htmlFor={itemHash} className="feature__label">
+                {item.name} ({USCurrencyFormat.format(item.cost)})
+            </label>
+            </div>
+    )}
+)};
+
+export default Parts;
